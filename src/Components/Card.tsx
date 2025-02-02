fffa40ea9ec65af7eb108822c5330b45583cdbcc
Jaydeep Godhani <jaydeep@walmart.com>
2025-02-02 16:58:17 +0530

    card fixed

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,7 +1,7 @@
 const Card = ({ header, desc}: {header: string, desc: string} ) => {
   return (
-    <div className='border-2 border-sky-700 p-2 h-56 shadow-xl hover:bg-sky-100 overflow-clip'>
-      <p className='text-2xl flex justify-start m-2 mb-3 underline underline-offset-8 decoration-sky-500 decoration-8'>{header}</p>
+    <div className='border-2 border-sky-700 p-2 h-56 shadow-xl hover:bg-sky-100 overflow-clip cursor-pointer'>
+      <p className='text-2xl flex justify-start m-2 mb-3 border-b-2 border-sky-100'>{header}</p>
 
       <div className='flex justify-start m-2'>{desc}</div>
     </div>
--- src/ViewAllVisa.tsx
+++ src/ViewAllVisa.tsx
@@ -6,11 +6,11 @@ const ViewAllVisa = () => {
       <div className='w-3/4'>
         <h1 className='text-2xl font-bold my-2'>All services</h1>
         <div className='h-auto grid grid-cols-3 gap-4'>
-          <Card header={'Test'} desc={'test test ??? long test and long text... lets see how it performs'}/>
-          <Card header={'Test'} desc={'test test ??? long test and long text... lets see how it performs'}/>
-          <Card header={'Test'}
+          <Card header={'Test test'} desc={'test test ??? long test and long text... lets see how it performs'}/>
+          <Card header={'Test jaydeep'} desc={'test test ??? long test and long text... lets see how it performs'}/>
+          <Card header={'Test hardik'}
                 desc={'test test ??? long test and long text... lets see how it performs test test ??? long test and long text... lets see how it performs test test ??? long test and long text... lets see how it performs test test ??? long test and long text...'}/>
-          <Card header={'Test'}
+          <Card header={'Test alpesh'}
                 desc={'test test ??? long test and long text... lets see how it performs test test ??? long test and long text... lets see how it performs test test ??? long test and long text... lets see how it performs test test ??? long test and long text...'}/>
           <Card header={'Test'}
                 desc={'test test ??? long test and long text... lets see how it performs test test ??? long test and long text... lets see how it performs test test ??? long test and long text... lets see how it performs test test ??? long test and long text...'}/>
